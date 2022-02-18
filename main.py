2f389050ebea1aea9832ac8c2d499b24ec864910
alex-1-tech <Alex225Games@yandex.ru>
2022-02-18 14:31:00 +0300

    change layers

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class Api_map:
         self.zoom = 12
         self.map_file = "map.png"
         self.type = "map"
+        self.map_layer_buttons = []
 
     def get_map(self):
         # get map screen
@@ -29,9 +30,9 @@ class Api_map:
             "ll": self.coord,
             "z": self.zoom,
             'l': self.type,
-            'controls': ['typeSelector']
         }
         response = requests.get(search_api_server, params=search_params)
+
         # recording a screenshot to a file
         with open(self.map_file, "wb") as file:
             file.write(response.content)
@@ -66,18 +67,28 @@ class Api_map:
         coord = str(x) + ',' + str(y)
         self.coord = coord
 
+    def pressed(self):
+        for t, button in enumerate(self.buttons):
+            if button.pressed(pygame.mouse.get_pos()):
+                if t == 0:
+                    self.type = "map"
+                elif t == 1:
+                    self.type = "sat"
+                elif t == 1:
+                    self.type = "skl"
+
     def create_buttons(self, screen, size) -> [Button, Button, Button]:
         scheme = Button()
         satellite = Button()
         hybrid = Button()
-
-        scheme_button = scheme.create_button(screen, (0, 0, 0),
-                                             size[0] - 135, 20, 120, 50, 100, 'Scheme', (255, 255, 255))
-        satellite_button = satellite.create_button(screen, (0, 0, 0),
-                                                   size[0] - 135, 100, 120, 50, 100, 'Satellite', (255, 255, 255))
-        hybrid_button = hybrid.create_button(screen, (0, 0, 0),
-                                             size[0] - 135, 180, 120, 50, 100, 'Hybrid', (255, 255, 255))
-        return scheme_button, satellite_button, hybrid_button
+        self.buttons = [scheme, satellite, hybrid]
+        self.map_layer_buttons.append(scheme.create_button(screen, (0, 0, 0),
+                                                           size[0] - 135, 20, 120, 50, 100, 'Scheme', (255, 255, 255)))
+        self.map_layer_buttons.append(satellite.create_button(screen, (0, 0, 0),
+                                                              size[0] - 135, 100, 120, 50, 100, 'Satellite',
+                                                              (255, 255, 255)))
+        self.map_layer_buttons.append(hybrid.create_button(screen, (0, 0, 0),
+                                                           size[0] - 135, 180, 120, 50, 100, 'Hybrid', (255, 255, 255)))
 
 
 def draw_map(y_map):
@@ -88,18 +99,19 @@ def draw_map(y_map):
     FPS = 60
     clock = pygame.time.Clock()
     running = True
-    map_layer_buttons = y_map.create_buttons(screen, size)
+    y_map.create_buttons(screen, size)
 
     while running:
         y_map.get_map()
         screen.blit(pygame.image.load(y_map.map_file), (0, 0))
         # draw buttons
-        for button in map_layer_buttons:
+        for button in y_map.map_layer_buttons:
             screen.blit(button, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                y_map.pressed()
         # take input
         direction_x, direction_y \
             = y_map.get_input(pygame.key.get_pressed())
